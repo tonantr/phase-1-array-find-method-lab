312f379642dbb7bfbb317e34dc83b74820c3c7d4
tonantr <tonantruong@hotmail.com>
2023-03-25 23:37:26 -0400

    complete Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+
+function superbowlWin(record) {
+    for (let item of record) {
+        if (item.result === 'W') {
+            return item.year;
+        };
+    }; 
+};
+
+record.find(superbowlWin);
